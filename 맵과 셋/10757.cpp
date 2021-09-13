9cd4497464621c4b454285a466d726e6338bfe13
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2021-09-14 00:00:09 +0900

    Create 10757.cpp

---
+++ 맵과 셋/10757.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string add(string A, string B){
+    int[] Asize = new int[A.size()+1];
+    int[] Bsize = new int[B.size()+1];
+}
+
+int main(){
+    string A, B;
+    cin >> A >> B;
+
+    add(A, B);
+
+    return 0;
+}
