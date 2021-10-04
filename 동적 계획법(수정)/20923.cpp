39e7b36c2153130c9319a0e17c82dd3e0bd088ca
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2021-10-04 16:38:58 +0900

    Create 20923.cpp

---
+++ 동적 계획법(수정)/20923.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K;
+int cnt[100005];
+int a[200005];
+
+int main() {
+    cin.tie(nullptr); 
+    ios::sync_with_stdio(false);
+
+    cin >> N >> K;
+    for(int i=1;i<=N;++i) cin >> a[i];
+    int s = 1, e = 1;
+    int ans = 0;
+    while(e <= N && s <= e) {
+        while(e <= N && cnt[a[e]] <= K) {
+            if(cnt[a[e]] == K) 
+                break;
+            cnt[a[e]]++;
+            ans = max(ans, e-s+1);
+            ++e;
+        }
+        while(s < e) {
+            if(cnt[a[s]] == K) {
+                --cnt[a[s++]];
+                break;
+            }
+            --cnt[a[s++]];
+        }
+    }
+    cout << ans << '\n';
+    return 0;
+}
